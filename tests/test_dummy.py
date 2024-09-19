425d60abe0b74f4502eca8b34b90a18015cdad3c
ekaterina.baru <ekaterina.baru@velotix.ai>
2024-09-19 16:32:44 +0300

    .

--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -1,8 +1,3 @@
-# This is a dummy test function to demonstrate pytest.
 def test_dummy():
     # Dummy condition, just to make the test pass.
     assert 1 + 1 == 2
-
-def test_fail_dummy():
-    # This will fail.
-    assert 1 + 1 == 3
